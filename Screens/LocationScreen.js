16ae422357cfa6d50abee4942fa04a6810ed381e
monishthirukachi <monishthirukachi@gmail.com>
2024-05-15 15:00:18 +0530

    thied commit

--- Screens/LocationScreen.js
+++ Screens/LocationScreen.js
@@ -1,79 +1,197 @@
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
-  Text,
   View,
+  Text,
   Button,
-  PermissionsAndroid,
-  Alert,
+  FlatList,
+  ScrollView,
 } from "react-native";
-import React from "react";
-import GetLocation from "react-native-get-location";
-import Geolocation from "@react-native-community/geolocation";
-Geolocation.setRNConfiguration(config);
+import * as Location from "expo-location";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const requestLocationPermission = async () => {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: "ReactNativeCode Location Permission",
-        message: "ReactNativeCode App needs access to your location ",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK",
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      Geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          console.log(latitude, longitude);z
-        },
-        {
-          enableHighAccuracy: false,
-          timeout: 60000,
-        }
-      );
-      // Geolocation.getCurrentPosition({
-      //     enableHighAccuracy: false,
-      //     timeout: 60000,
-      // })
-      // .then(location => {
-      //     console.log(location);
-      // })
-      // .catch(error => {
-      //     const { code, message } = error;
-      //     console.warn(code, message);
-      // })
 
-      Alert.alert("Location Permission Granted.");
-    } else {
-      Alert.alert("Location Permission Not Granted");
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-};
+
 const LocationScreen = () => {
-  return (
-    <View style={styles.container}>
-      <View style={{ margin: 50 }}>
-        <Button
-          title="request permission"
-          onPress={requestLocationPermission}
-        ></Button>
-      </View>
+  const [location, setLocation] = useState(null);
+  const [isRunning, setIsRunning] = useState(false); // Initially, the background task is running
+  const [locationData, setLocationData] = useState([]);
+
+  // Function to store location data in AsyncStorage
+  const storeLocationData = async (data) => {
+    try {
+      const existingData = await AsyncStorage.getItem("locationQueue");
+      let locationQueue = existingData ? JSON.parse(existingData) : [];
+      locationQueue.push(data);
+      await AsyncStorage.setItem(
+        "locationQueue",
+        JSON.stringify(locationQueue)
+      );
+    } catch (error) {
+      console.error("Error storing location data:", error);
+    }
+  };
+
+  // const storeRetrievedData = async (data) => {
+  //   try {
+  //     let locationQueue = [];
+  //     if (fs.existsSync(filePath)) {
+  //       const existingData = fs.readFileSync(filePath, 'utf8');
+  //       locationQueue = existingData ? JSON.parse(existingData) : [];
+
+  //       locationQueue.push(data);
+
+  //       fs.writeFileSync(filePath, JSON.stringify(locationQueue, null, 2));
+
+  //       console.log('Data stored successfully');
+  //     }
+  //   } catch (error) {
+  //     console.error('Error storing location data:', error);
+  //   }
+  // }
+
+  // Function to retrieve stored location data from AsyncStorage
+  const retrieveLocationData = async () => {
+    try {
+      const data = await AsyncStorage.getItem("locationQueue");
+      if (data) {
+        setLocationData(JSON.parse(data));
+      }
+    } catch (error) {
+      console.error("Error retrieving location data:", error);
+    }
+  };
+
+  const handleStart = async () => {
+    await AsyncStorage.removeItem("locationQueue");
+    setIsRunning(true);
+    // getLocation();
+  };
+
+  // Function to get location and handle storage
+  const getLocation = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Highest,
+      });
+      setLocation(location);
+      const isoString = new Date().toISOString();
+      const [da, ti] = isoString.split("T");
+      const time = `${da} ${ti}`;
+
+      storeLocationData({
+        time: time,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    } catch (error) {
+      console.error("Error getting location:", error);
+    }
+  };
+
+  // Background task to get location every 5 minutes
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        getLocation();
+      }, 1000); // 5 minutes interval
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+  
+  // Function to handle stop button press
+  const handleStop = async () => {
+    setIsRunning(false); // Stop the background task
+    retrieveLocationData();
+    // storeRetrievedData(locationData);
+    // Retrieve stored location data
+  };
+
+  // Render function for each item in the FlatList
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>Time: {item.time}</Text>
+      <Text>Latitude: {item.latitude}</Text>
+      <Text>Longitude: {item.longitude}</Text>
     </View>
+  );
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Welcome!</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Start" onPress={handleStart} style={styles.button} />
+        </View>
+        {/* <View style={styles.buttonContainer} >
+        <Button title="Get Location" onPress={getLocation} style={styles.button} />
+      </View>
+      {location && (
+        <>
+          <Text>Latitude: {location.coords.latitude}</Text>
+          <Text>Longitude: {location.coords.longitude}</Text>
+        </>
+      )} */}
+        <View style={styles.buttonContainer}>
+          <Button title="Stop" onPress={handleStop} style={styles.button} />
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        {locationData.length > 0 && (
+          <FlatList
+            // style={{ alignItems:'center' }}
+            data={locationData}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
+      </View>
+    </>
   );
 };
 
-export default LocationScreen;
-
 const styles = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    marginBottom: 10,
+    backgroundColor: "#fff",
+  },
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#fff",
     alignItems: "center",
-    // margin: 50,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 10,
+    width: "40%",
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  button: {
+    backgroundColor: "#313590",
+    marginTop: 25,
+    // margin: 10,
+    borderRadius: 10,
+    width: 100,
+    padding: 10,
+    alignItems: "center",
   },
 });
+
+export default LocationScreen;
--- assets/constants.js
+++ assets/constants.js
@@ -1,3 +1,5 @@
 export const userName = "1";
-export const passWord = "1"
+export const passWord = "1";
 export const id = 1;
+export const lat = "50.5";
+export const long = "101.122";
